0d759b9e13d16fdfe5997e01876c90c97dd32474
Olga Manukhova <olga.manukhova@gmail.com>
2016-11-03 14:42:12 +0300

    main commit

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,14 @@
 
 require('./upload');
 var load = require('./load');
-var Gallery = require('./gallery');
-var getPictureElement = require('./get-picture-element');
 
+var getPictures = require('./pictures');
 
 var filtersHidden = document.querySelector('.filters');
 
 filtersHidden.classList.add('hidden');
 
 load('//localhost:1507/api/pictures', function(pictures) {
-
-  Gallery.setPictures(pictures);
-  pictures.forEach(getPictureElement);
-
-
+  getPictures(pictures);
   filtersHidden.classList.remove('hidden');
 });
